622c62b5afe86bbb4f0da446b7c6a9929d15c87b
byhelaman <byhelaman@gmail.com>
2025-02-14 11:41:53 -0500

    fix config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'g&9kYT1GBvhP@K9H'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'g&9kYT1GBvhP@K9H')
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
     TEMP_FOLDER = os.path.join(os.getcwd(), 'temp')
